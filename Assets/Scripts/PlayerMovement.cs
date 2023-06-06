4ee3d602d0b6d80caef3ae3e55feb4deeb56cb32
Davidnw78 <davidnw78@gmail.com>
2023-06-06 19:11:38 -0400

    Trying to add sprint
    -Changed speed to 6f.
    -Added OnSprint method.
    
    (Fix this)
    Pressing left shift changes multiplier to 1.5f after releasing shift, it stays 1.5f

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -40,3 +41,52 @@ public class PlayerMovement : MonoBehaviour
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private int speed = 6;
+    [SerializeField] private float multiplier = 1f;
+    // Set Vector2 name same as PlayerInput/Actions name
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator animator;
+    
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnSprint (InputValue sprintInput)
+    {   
+        var isSprinting = sprintInput.Get<float>();
+        multiplier = isSprinting > 0 ? 1.5f : 1.0f;
+    }
+    private void OnMovement (InputValue value) 
+    {
+        movement = value.Get<Vector2>();
+        if(movement.x != 0 || movement.y != 0)
+        {
+            animator.SetFloat("X", movement.x);
+            animator.SetFloat("Y", movement.y);
+
+            animator.SetBool("IsWalking", true);
+        }    
+        else
+        {
+            animator.SetBool("IsWalking", false);    
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * multiplier * Time.fixedDeltaTime);
+    }
+}
+>>>>>>> Stashed changes
